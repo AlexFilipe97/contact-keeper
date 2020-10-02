5420805dea2543d9362bd3e96f2a7d73b636ff5d
AlexFilipe97 <alex.filipe97@gmail.com>
2020-10-02 18:13:24 +0100

    Added json token validation, contact schema, new contact POST and user's contacts GET

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const { genSalt } = require("bcryptjs");
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
