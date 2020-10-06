d3e7f6a5c1c1ce7056886571a7c5d92912eb810b
AlexFilipe97 <alex.filipe97@gmail.com>
2020-10-06 18:31:48 +0100

    Added context for contacts and created components Contacts, ContactItem and ContactForm

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,77 @@
+import React, { useReducer } from "react";
+import { v4 as uuid } from "uuid";
+import ContactContext from "./ContactContext";
+import ContactReducer from "./ContactReducer";
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from "../types";
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: "Jill Johnson",
+        email: "jillJ@gmail.com",
+        phone: "111-111-1111",
+        type: "personal",
+      },
+      {
+        id: 2,
+        name: "Sarah Watson",
+        email: "sarahW@gmail.com",
+        phone: "222-222-2222",
+        type: "personal",
+      },
+      {
+        id: 3,
+        name: "Harry White",
+        email: "harryW@gmail.com",
+        phone: "444-444-4444",
+        type: "professional",
+      },
+    ],
+  };
+
+  const [state, dispatch] = useReducer(ContactReducer, initialState);
+
+  // add contact
+  const addContact = (contact) => {
+    contact.id = uuid();
+    dispatch({
+      type: ADD_CONTACT,
+      payload: contact,
+    });
+  };
+
+  // delete contact
+
+  // set current contact
+
+  // clear current contact
+
+  // update contact
+
+  // filter contacts
+
+  // clear filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+        addContact,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
